65131be4e0530c32b4055a038ebd1766da428099
Sonu <sahanawazalamnaruto@gmail.com>
2025-03-09 18:27:25 +0530

    first commit

---
+++ src/components/BentoTilt.jsx
@@ -0,0 +1,34 @@
+import { useRef, useState } from "react";
+
+const BentoTilt = ({ children, className=""}) => {
+    const [transformStyle, setTransformStyle] = useState('');
+    const itemRef = useRef();
+
+    const handleMouseMove = (e) => {
+        if(!itemRef.current) return;
+
+        const { left, top, width, height } = itemRef.current.getBoundingClientRect();
+
+        const relativeX = (e.clientX - left) / width;
+        const relativeY = (e.clientY - top) / height;
+
+        const tiltX = (relativeX - 0.5) * 50;
+        const tiltY = (relativeY - 0.5) * -50;
+
+        const newTransform = `perspective(700px) rotateX(${tiltY}deg) rotateY(${tiltX}deg) scale3d(0.95, 0.95, 0.95)`;
+
+        setTransformStyle(newTransform);
+    }
+
+    const handleMouseLeave = (e) => {
+        setTransformStyle('');
+    }
+
+    return (
+        <div className={className} ref={itemRef} onMouseMove={handleMouseMove} onMouseLeave={handleMouseLeave} style={{transform: transformStyle}}>
+            {children}
+        </div>
+    )
+}
+
+export default BentoTilt
